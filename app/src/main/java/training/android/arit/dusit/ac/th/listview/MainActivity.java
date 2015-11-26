e955212f97a8b109879c6efd05c6c936ddcd82db
tpsumeta <tpsumeta@gmail.com>
2015-11-26 13:50:04 +0700

    add setOnItemClickListener

--- app/src/main/java/training/android/arit/dusit/ac/th/listview/MainActivity.java
+++ app/src/main/java/training/android/arit/dusit/ac/th/listview/MainActivity.java
@@ -2,6 +2,8 @@ package training.android.arit.dusit.ac.th.listview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -16,9 +18,30 @@ public class MainActivity extends AppCompatActivity {
 
         listView = (ListView) findViewById(R.id.listView);
 
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,menu1);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, menu1);
         listView.setAdapter(adapter);
 
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> arg0, View arg1
+                    , int arg2, long arg3) {
+                if (arg2 == 0) {
+
+                }
+                if (arg2 == 1) {
+
+                }
+                if (arg2 == 2) {
+
+                }
+                if (arg2 == 3) {
+
+                }
+                if (arg2 == 4) {
+
+                }
+
+            }
+        });
 
 
     }
